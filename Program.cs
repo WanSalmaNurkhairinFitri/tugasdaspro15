61e998a6a93feee8a7abfeaf2c02bb8b9158a806
WanSalmaNurkhairinFitri <120373463+WanSalmaNurkhairinFitri@users.noreply.github.com>
2022-12-16 23:01:41 +0700

    Wan Salma Nurkhairin Fitri-2207125057

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Tiket_Bioskop
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            string Harga;
+            Console.Write("Nama         : ");
+            string Nama = Console.ReadLine();
+            Console.Write("Tahun Lahir  : ");
+            int ThnLahir = Convert.ToInt32(Console.ReadLine());
+
+            int Usia = 2022 - ThnLahir;
+
+           if(Usia < 10 || Usia > 60)
+                {
+                    Harga = "Rp. 10.000,-";
+                }
+                else
+                {
+                    Harga = "Rp. 25.000,-";
+                }
+                Console.WriteLine("|****************************|");
+                Console.WriteLine(String.Format("| {0,-18}{1,-15}       |", "Nama  : ", Nama));
+                Console.WriteLine(String.Format("| {0,-11}{1,-10}     |", "Harga : ", Harga));
+                Console.WriteLine("+----------------------------+");
+
+                
+            
+        }  
+    }
+}
+
+        
+    
+
+   
+
